0992053244fbfba8bf29a2ba99b5bad2c2e9836f
slubwama63-afk <slubwama63@gmail.com>
2025-10-03 14:31:10 +0200

    Completed Assignment 2
    Valde att testa adventure game :)

--- js/app.js
+++ js/app.js
@@ -187,7 +187,7 @@ listMove.forEach (char=>{
   }}
 ) */
 
-function doubleNumber(numberToDouble) {
+/*function doubleNumber(numberToDouble) {
   return numberToDouble * 2;
 }
 console.log(doubleNumber(2));
@@ -198,57 +198,34 @@ function doubleNumber2(numberToDouble) {
 console.log(doubleNumber2(8));
 
 function dubbelNumber (number1,number2) {
-  return number1 * number2;
+  return number1 - number2;
 }
-console.log(dubbelNumber(4,6));
+console.log(dubbelNumber(6,2));
 
+function welcome (firstName, lastName) {
+  return "Welcome " + firstName + " " + lastName + "!";
+}
+console.log(welcome("Ben","Ting"))
 
+function countLetters( name ) {
+  return name.length;
+}
+console.log(countLetters("Michelle Linnea"));
 
+function getCapitalize (firstName, lastName) {
+  let first = firstName [0].toUpperCase() + firstName.slice(1). toLowerCase();
+  let last = lastName [0].toUpperCase() + lastName.slice(1).toLowerCase();
+  return first + " " + last;
+}
+console.log(getCapitalize("sandra" , "lubwama"));
 
+function phrase (originalString, reversedString) {
+  let orginalString = "Hello";
+  let reversedString = "Olleh"
+  return originalString + reversedString;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+console.log(phrase("Hello" + reversedString"Olleh")); */
 
 /*let kmPerHour = 72; //input
 let metersPerSecond = kmPerHour / 3.6; // conversion
@@ -324,6 +301,133 @@ console.log(hypotenuse); */
 const inputSeconds = 111;
 const result = convertSeconds(inputSeconds);
 console.log(result); */
+
+
+// Step 1: Character Setup
+const playerName = "Sandri";
+let playerAge = 16;
+let rightHand = "sword";
+let leftHand = "shield";
+
+// Step 2: Functions
+function checkEligibility(age) {
+  if (age < 12) {
+    return "Too young to start the adventure!";
+  } else {
+    return "You are ready for the quest!";
+  }
+}
+
+function chooseEquipment(rightHand, leftHand) {
+  if (rightHand && leftHand) {
+    return "You are armed with a " + rightHand + " and a " + leftHand + "!";
+  } else if (rightHand) {
+    return "You only have a " + rightHand + ".";
+  } else if (leftHand) {
+    return "You only have a " + leftHand + ".";
+  } else {
+    return "You are unarmed! Be careful!";
+  }
+}
+
+// Step 3: Conditions & Booleans
+function startingLocation(age) {
+  if (age < 12) {
+    return "You start in your house.";
+  } else if (age >= 12 && age <= 17) {
+    return "You start in beginners forest.";
+  } else {
+    return "You start in a dark dungeon.";
+  }
+}
+
+//Step 5 Bonus
+function rollDice() {
+  return Math.floor(Math.random() *6) + 1;
+}
+//Step 4
+function main(name, age, rightHand, leftHand) {
+  console.log("Welcome, " + name + " ! ");
+  console.log(checkEligibility(age));
+  console.log(startingLocation(age));
+  console.log(chooseEquipment(rightHand, leftHand));
+}
+
+main(playerName, playerAge, rightHand, leftHand);
+
+
+// Bonus: slå tärning
+let dice = rollDice();
+console.log("You rolled a " + dice + ".");
+if (dice >= 4) {
+  console.log("A monster appears!");
+} else {
+  console.log("The path is clear... for now.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
